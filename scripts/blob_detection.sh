1bba33a3e50d467826ce87261d1e5ef78deaefd7
radio1988 <liruiradiant@gmail.com>
2020-01-30 14:43:54 -0500

    works; organized folder

--- scripts/blob_detection.sh
+++ scripts/blob_detection.sh
@@ -1,9 +1,17 @@
-# czi file names must have no spaces
+#!/bin/bash
 
-for file in ../test/*czi ../CFUe_P8-9/*czi
+# czi file names must have no spaces
+for file in ../../ccount_data/IL17a_CFUe_24JAN20/*czi
 do 
-echo $file
-fname=$file runipy blob_detection.ipynb ${file}.ipynb 1> $file.log 2> $file.err
-jupyter nbconvert --to html ${file}.ipynb
+name=$(basename $file)
+name=${name/.czi/}
+echo ">>> For" $file $name
+fname=$file runipy blob_detection.ipynb ${name}.ipynb  # work
+jupyter nbconvert --to html ${name}.ipynb && rm -f ${name}.ipynb # report html then delete
+mkdir -p report
+mkdir -p blobs
+mv ${name}.html report
+mv ${name}.npy blobs
+pigz blobs/${name}.npy &
 done
 
