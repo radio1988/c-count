ab022abc700a49bbe0df963c11fdf7225a2f36c0
radio1988 <liruiradiant@gmail.com>
2020-03-20 14:41:29 -0400

    added snakemake

---
+++ scripts/submit.sh
@@ -0,0 +1,2 @@
+nohup snakemake --use-conda -k --jobs 999 --latency-wait 14400 \
+--cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake --report report.html &
