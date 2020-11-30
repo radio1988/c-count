52a1ee66352214ac59813002389e8eb2ee1214fb
radio1988 <liruiradiant@gmail.com>
2020-11-30 03:29:35 -0500

    less wait for output

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,2 +1,6 @@
-nohup snakemake -s scripts/Snakefile -k --jobs 999 --latency-wait 14400 \
+#cd workdir
+#ln -s $path/scripts
+#cp scripts/config.yaml ./ # then edit
+
+nohup snakemake -s scripts/Snakefile -k --jobs 999 --latency-wait 60 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake -j 1 --report report.html &
