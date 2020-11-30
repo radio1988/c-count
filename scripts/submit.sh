06ce4caa8de1f6977c07bad7632f947a79847c74
radio1988 <liruiradiant@gmail.com>
2020-11-30 01:59:41 -0500

    keep Snakefile in scripts/ folder

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,2 +1,2 @@
-nohup snakemake -k --jobs 999 --latency-wait 14400 \
+nohup snakemake -s scripts/Snakefile -k --jobs 999 --latency-wait 14400 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake -j 1 --report report.html &
