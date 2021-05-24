307c362b71d208aefe7c1a8b9e38ab4ea933f89a
radio1988 <liruiradiant@gmail.com>
2021-05-24 06:46:08 -0400

    cleanup

---
+++ workflow/submit.sh
@@ -0,0 +1,6 @@
+#cd workdir
+#ln -s $path/scripts
+#cp scripts/config.yaml ./ # then edit
+
+nohup snakemake -k --jobs 999 --latency-wait 60 \
+--cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake -j 1 --report report.html &
