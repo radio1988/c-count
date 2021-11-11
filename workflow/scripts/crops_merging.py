24b9aaf5e07b214de76a118806d764d9986b5c02
radio1988 <liruiradiant@gmail.com>
2021-11-11 15:38:06 -0500

    crops_merging.py: files separated by space

--- workflow/scripts/crops_merging.py
+++ workflow/scripts/crops_merging.py
@@ -8,7 +8,7 @@ def parse_cmd_and_prep ():
     # Construct the argument parser and parse the arguments
     parser = argparse.ArgumentParser(
         description='reads crops.npy.gz, set label, output')
-    parser.add_argument("-crops", type=str,
+    parser.add_argument("-crops", type=str,nargs='+',
                     help="blob-crops files, e.g. name1.crops.npy.gz,name2.crops.npy.gz")
     parser.add_argument("-output", type=str,
                     help="output, e.g. merged.crops.npy.gz")
@@ -24,9 +24,8 @@ def parse_cmd_and_prep ():
 
 
 args = parse_cmd_and_prep()
-crop_names = args.crops.split(',')
 
-for i, crop_name in enumerate(crop_names):
+for i, crop_name in enumerate(args.crops):
     crops = load_crops(crop_name)
     if i == 0:
         output_crops = crops
