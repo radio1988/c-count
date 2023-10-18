d76b81abc1f0cf5b0e9cbbb01ecd35fb134b28f5
radio1988 <liruiradiant@gmail.com>
2023-10-18 14:36:54 -0400

    crops_stats.py

---
+++ workflow/scripts/crops_stats.py
@@ -0,0 +1,22 @@
+import argparse, textwrap
+from ccount.blob.io import load_crops, save_crops
+
+
+def parse_cmd_and_prep ():
+    # Construct the argument parser and parse the arguments
+    parser = argparse.ArgumentParser(
+    	formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        	crops_stats.py -crops crops.labled.npy.gz
+        	'''))
+
+    parser.add_argument("-crops", type=str,
+        help="blob-crops file, e.g. labeled/labeled.crops.npy.gz")
+
+    args = parser.parse_args()
+    print('\n'.join(f'{k}={v}' for k, v in vars(args).items()))
+    return args
+
+
+args = parse_cmd_and_prep()
+crops = load_crops(args.crops)
