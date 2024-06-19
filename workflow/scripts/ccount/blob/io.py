6224c10fb97b2ac0b834f76c6d88bb9013848715
rui <liruiradiant@gmail.com>
2024-06-19 09:36:45 -0400

    save locs

--- workflow/scripts/ccount/blob/io.py
+++ workflow/scripts/ccount/blob/io.py
@@ -1,13 +1,13 @@
 import gzip, os, subprocess
 import numpy as np
+from .misc import crops_stat, crop_width
+
 
 def load_locs(fname):
     '''
     assuming reading fname.locs.npy.gz
     read into np.array
     '''
-
-
     if os.path.isfile(fname):
         if fname.endswith('npy'):
             array = np.load(fname)
@@ -15,48 +15,35 @@ def load_locs(fname):
             f = gzip.GzipFile(fname, "r")
             array = np.load(f)
         else:
-            raise Exception ("suffix not npy nor npy.gz")
+            raise Exception("suffix not npy nor npy.gz")
         print(fname, array.shape)
         return array
     else:
-        raise Exception ('input file', fname, 'not found')
+        raise Exception('input file', fname, 'not found')
 
 
-def load_crops(in_db_name, n_subsample=False, seed=1):
+def load_crops(in_db_name):
     '''
     use parameters: db_name
     input: db fname from user (xxx.npy)
     output: array (crops format)
     '''
-    from .misc import crops_stat, crop_width, sub_sample
+    image_flat_crops = load_locs(in_db_name)
 
-    if not os.path.isfile(in_db_name):
-        raise Exception("{} file not found".format(in_db_name))
+    if (image_flat_crops.shape[1] > 3):  # with label
+        crops_stat(image_flat_crops)
 
-    print("Reading {}".format(in_db_name))
-    if in_db_name.endswith('npy'):
-        image_flat_crops = np.load(in_db_name)
-    elif in_db_name.endswith('npy.gz'):
-        f = gzip.GzipFile(in_db_name, "r")
-        image_flat_crops = np.load(f)
-    else:
-        raise Exception ("db suffix not npy nor npy.gz")
+    if (image_flat_crops.shape[1] > 4 + 4):
+        print("n-crop: {}, crop width: {}". \
+              format(len(image_flat_crops), crop_width(image_flat_crops)))
 
-    if n_subsample:
-        print("subsampling to", n_subsample, "blobs")
-        image_flat_crops = sub_sample(image_flat_crops, n_subsample, seed=seed)  
-
-    # print("n-crop: {}, crop width: {}".\
-    #     format(len(image_flat_crops), crop_width(image_flat_crops)))
-    # crops_stat(image_flat_crops)
-    
     return image_flat_crops
 
 
 def save_crops(crops, fname):
     from .misc import crops_stat, crop_width
     from pathlib import Path
-    print("Saving", fname)
+    print("Saving crops:", fname)
     Path(os.path.dirname(fname)).mkdir(parents=True, exist_ok=True)
     crops_stat(crops)
     print('crops dim:', crops.shape)
@@ -69,10 +56,10 @@ def save_crops(crops, fname):
 def save_locs(crops, fname):
     from .misc import crops_stat, crop_width
     from pathlib import Path
-    print("Saving", fname)
+    crops = crops[:, 0:4]
+    print("Saving locs:", fname)
     Path(os.path.dirname(fname)).mkdir(parents=True, exist_ok=True)
     print('crops dim:', crops.shape)
     fname = fname.replace(".npy.gz", ".npy")
     np.save(fname, crops)
     subprocess.run("gzip -f " + fname, shell=True, check=True)
-
