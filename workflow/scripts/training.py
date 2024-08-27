a8974467419d58349b53709995fe0bcd71cd4312
rui <liruiradiant@gmail.com>
2024-08-27 02:05:06 -0400

    train: augment_and_balance

--- workflow/scripts/training.py
+++ workflow/scripts/training.py
@@ -1,3 +1,5 @@
+import random
+
 from ccount.img.equalize import equalize
 from ccount.img.auto_contrast import float_image_auto_contrast
 from ccount.img.transform import down_scale
@@ -131,7 +133,41 @@ valrs = valrs / config['clas_scaling_factor']
 
 # augmentation
 print(">>> Before Aug:", trainimages.shape, trainrs.shape, trainlabels.shape)
-trainimages, trainlabels, trainrs = augment_crops(trainimages, trainlabels, trainrs,
+
+def augment_and_balance(images, labels, rs, N):
+    '''
+    input: [images, labels, rs, N], N is the goal of N images
+    output: [images, labels, rs], balanced and augmented if N > n(images)
+    process:
+    - only consider POS and NEG, others discarded
+    - POS and NEG augmented to N//2 separately
+    - POS and NEG Merged and shuffled
+    '''
+    pos_idx = labels == 1
+    neg_idx = labels == 0
+    images_pos = images[pos_idx, ]
+    images_neg = images[neg_idx, ]
+    labels_pos = labels[pos_idx]
+    labels_neg = labels[neg_idx]
+    rs_pos = rs[pos_idx]
+    rs_neg = rs[neg_idx]
+
+    images_pos, labels_pos, rs_pos = augment_crops(images_pos, labels_pos, rs_pos, N//2)
+    images_neg, labels_neg, rs_neg = augment_crops(images_neg, labels_neg, rs_neg, N//2)
+
+    images = np.vstack(images_pos, images_neg)
+    labels = labels_pos + labels_neg
+    rs = rs_pos + rs_neg
+
+    rnd_idx = random.shuffle(range(len(labels)))
+    images = images[rnd_idx, ]
+    labels_pos = labels[rnd_idx]
+    rs = rs[rnd_idx]
+
+    return images, labels, rs
+
+
+trainimages, trainlabels, trainrs = augment_and_balance(trainimages, trainlabels, trainrs,
                                                   config['aug_sample_size'])
 print("After Aug:", trainimages.shape, trainrs.shape, trainlabels.shape)
 print('pixel value max', np.max(trainimages), 'min', np.min(trainimages))
