6a5621e045c4049fb144eeb9b1f0ee685f73f7a4
rui <liruiradiant@gmail.com>
2024-11-27 23:54:20 -0500

    blob_detection.py: documentation added

--- workflow/scripts/blob_detection.py
+++ workflow/scripts/blob_detection.py
@@ -1,5 +1,6 @@
 import matplotlib
-matplotlib.use('Agg') 
+
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from ccount.img.equalize import block_equalize
 from ccount.img.read_czi import read_czi, parse_image_obj
@@ -15,19 +16,29 @@ from pathlib import Path
 import argparse, os, re, yaml
 import numpy as np
 
-# usage: 
-# python workflow/scripts/blob_detection.py -i data/IL17A_POINT1_EPO_1.czi \
-# -c config.yaml -odir res/blob_locs/
+"""
+Input: czi
+Output: locs.gz or crops.gz for all scenes available in the czi (4 or less)
 
-def parse_cmd_and_prep ():
+params:
+-i: input czi file
+-c: config.yaml for blob detection params
+-odir: output dir
+
+example usage:
+python workflow/scripts/blob_detection.py -i data/IL17A_POINT1_EPO_1.czi -c config.yaml -odir res/blob_locs/
+"""
+
+
+def parse_cmd_and_prep():
     parser = argparse.ArgumentParser(
         description='Read czi, output blobs')
     parser.add_argument('-i', type=str,
-        help='input file name: path/xxx.czi')
-    parser.add_argument('-c', type=str, default="config.yaml", 
-        help='path to config.yaml file')
+                        help='input file name: path/xxx.czi')
+    parser.add_argument('-c', type=str, default="config.yaml",
+                        help='path to config.yaml file')
     parser.add_argument('-odir', type=str, default="blobs",
-        help='output dir: default blobs'        )
+                        help='output dir: default blobs')
 
     args = parser.parse_args()
     print('input fname:', args.i)
@@ -38,69 +49,67 @@ def parse_cmd_and_prep ():
 
     with open(args.c, 'r') as stream:
         config = yaml.safe_load(stream)
-    if config['blob_detection_scaling_factor'] not in [1,2,4]:
+
+    if config['blob_detection_scaling_factor'] not in [1, 2, 4]:
         raise Exception(
-            'scaling_factor', config['blob_detection_scaling_factor'], 
+            'scaling_factor', config['blob_detection_scaling_factor'],
             'not implemented, only support 1,2,4'
-            )
+        )
 
     return [args, corename, config]
 
 
-##################Start####################
 [args, corename, config] = parse_cmd_and_prep()
 
-image_obj = read_czi(args.i, Format=config['FORMAT'])  # fast already
+image_obj = read_czi(args.i, Format=config['FORMAT'])
+
 for i in range(len(image_obj.scenes)):
-    # names
-    i=str(i)
-    out_blob_fname = os.path.join(args.odir, corename+"."+i+".locs.npy")
+    i = str(i)
+    out_blob_fname = os.path.join(args.odir, corename + "." + i + ".locs.npy")
     print("Searching blobs for area", i, "; output:", out_blob_fname)
 
-    image_obj = read_czi(args.i, Format=config['FORMAT'])  # fast already
+    image_obj = read_czi(args.i, Format=config['FORMAT'])
     image = parse_image_obj(image_obj, i=i, Format=config['FORMAT'])
-    image = uint16_image_auto_contrast(image) # still uint16
+    image = uint16_image_auto_contrast(image)
 
     if config['test_mode']:
         print("in test mode")
         image = image[0:3000, 0:3000]
 
     image_equ = block_equalize(
-        image, 
-        block_height=config['block_height'], 
-        block_width=config['block_width'])
+        image,
+        block_height=config['block_height'],
+        block_width=config['block_width']
+    )
+
     image_equ = image_equ.astype(np.single)  # save RAM
 
-    blob_locs = find_blob (
-        1-image_equ, 
+    blob_locs = find_blob(
+        1 - image_equ,
         scaling_factor=config['blob_detection_scaling_factor'],
-        max_sigma=config['max_sigma'], 
-        min_sigma=config['min_sigma'], 
-        num_sigma=config['num_sigma'], 
-        threshold=config['threshold'], 
+        max_sigma=config['max_sigma'],
+        min_sigma=config['min_sigma'],
+        num_sigma=config['num_sigma'],
+        threshold=config['threshold'],
         overlap=config['overlap'],
-        )
-    print('there are {} blobs detected'.format(blob_locs.shape[0]))
+    )
+    print('there are {} blobs detected\n'.format(blob_locs.shape[0]))
+
     save_locs(blob_locs, out_blob_fname)
 
-
     # Visualizing filtered blobs
-    # todo: split into another script to avoid RAM crash
+    # todo: split into another script to avoid RAM crash (curretly 4.5G usage, may reduce to 2.5G)
     if config['blob_detection_visualization']:
         Path(os.path.join(args.odir, "vis_blob_detection")).mkdir(parents=True, exist_ok=True)
-        out_img_fname = os.path.join(args.odir, "vis_blob_detection", corename+"."+i+".jpg")
+        out_img_fname = os.path.join(args.odir, "vis_blob_detection", corename + "." + i + ".jpg")
         print("output_img_fname:", out_img_fname)
         visualize_blob_detection(image, blob_locs,
-            blob_extention_ratio=config['blob_extention_ratio'], 
-            blob_extention_radius=config['blob_extention_radius'], 
-            fname=out_img_fname)
+                                 blob_extention_ratio=config['blob_extention_ratio'],
+                                 blob_extention_radius=config['blob_extention_radius'],
+                                 fname=out_img_fname
+                                 )
 
-    # test: skipped edge filter (we have neg plates now), 08/23/21
-    # good_flats = remove_edge_crops(image_flat_crops)
-    # print("there are {} blobs passed edge filter".format(len(good_flats)))
-    # good_flats = image_flat_crops
-
-
-
-
-
+# test: skipped edge filter (we have neg plates now), 08/23/21
+# good_flats = remove_edge_crops(image_flat_crops)
+# print("there are {} blobs passed edge filter".format(len(good_flats)))
+# good_flats = image_flat_crops
