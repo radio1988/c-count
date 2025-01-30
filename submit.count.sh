ec16da2d8b2da7800b5170f505ed2a8bbd15629b
rui <liruiradiant@gmail.com>
2025-01-30 12:03:16 -0500

    submit count train sh

--- submit.count.sh
+++ submit.count.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # run with:
-# bsub -q long -W 144:00 -R rusage[mem=4000] 'bash submit.train.sh'
+# bsub -q long -W 144:00 -R rusage[mem=4000] 'bash submit.count.sh'
 
 source /home/rui.li-umw/anaconda3/etc/profile.d/conda.sh
-conda activate mamba > train.log 2>&1
+conda activate mamba > count.log 2>&1
 
 snakemake \
 -s workflow/count.Snakefile \
 --rerun-triggers mtime \
 -p -k --jobs 999 \
 --ri --restart-times 1 \
---cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 144:00' > train.log 2>&1
+--cluster 'bsub -q long -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 144:00' > count.log 2>&1
+
+snakemake -j 1 --report report.html >> count.log  2>&1
