1d6781771f091a0b2f0d7fa6dfcdb7248ef0da87
rui <liruiradiant@gmail.com>
2025-03-24 12:23:57 -0400

    update comments

--- resources/test_runs/1_training_test/run_training.sh
+++ resources/test_runs/1_training_test/run_training.sh
@@ -1,3 +1,4 @@
 conda activate c-count-env
-snakemake -s workflow/train.Snakefile -pk --ri -j1
-snakemake -s workflow/train.Snakefile -j 1 --report report.html
+snakemake -s workflow/train.Snakefile -j1 reset  # remove all previous results
+snakemake -s workflow/train.Snakefile -pk --ri -j1  # run training on example data
+snakemake -s workflow/train.Snakefile -j 1 --report report.html  # generate report on the run-time
